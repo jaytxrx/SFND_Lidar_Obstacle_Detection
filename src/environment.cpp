3102d2b01bb0fc76b3d6a6c9a8f13196bf5b79f5
Jeffrey Antony <jeffrey@jeffrey.co.in>
2020-12-11 11:11:38 +0100

    PCD Stream

--- src/environment.cpp
+++ src/environment.cpp
@@ -75,12 +75,12 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     }
 }
 
-
-void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer)
+void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer, ProcessPointClouds<pcl::PointXYZI> processPointCloudsI, const pcl::PointCloud<pcl::PointXYZI>::Ptr& inputCloud)
+//void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer)
 {
-    ProcessPointClouds <pcl::PointXYZI> processPointCloudsI;
+    //ProcessPointClouds <pcl::PointXYZI> processPointCloudsI;
 
-    pcl::PointCloud<pcl::PointXYZI>::Ptr inputCloud = processPointCloudsI.loadPcd("../src/sensors/data/pcd/data_1/0000000000.pcd");
+    //pcl::PointCloud<pcl::PointXYZI>::Ptr inputCloud = processPointCloudsI.loadPcd("../src/sensors/data/pcd/data_1/0000000000.pcd");
 
     // Experiment with the ? values and find what works best
     pcl::PointCloud<pcl::PointXYZI>::Ptr filterCloud = processPointCloudsI.FilterCloud(inputCloud, 0.2f , Eigen::Vector4f (-10,-5,-2,1), Eigen::Vector4f (25,7,1,1 ));
@@ -141,10 +141,26 @@ int main (int argc, char** argv)
     CameraAngle setAngle = XY;
     initCamera(setAngle, viewer);
     //simpleHighway(viewer);
-    cityBlock(viewer);
+
+    ProcessPointClouds<pcl::PointXYZI> pointProcessorI;
+    std::vector<boost::filesystem::path> stream = pointProcessorI.streamPcd("../src/sensors/data/pcd/data_1");
+    auto streamIterator = stream.begin();
+    pcl::PointCloud<pcl::PointXYZI>::Ptr inputCloudI;
 
     while (!viewer->wasStopped ())
     {
+        // Clear viewer
+        viewer->removeAllPointClouds();
+        viewer->removeAllShapes();
+
+        // Load pcd and run obstacle detection process
+        inputCloudI = pointProcessorI.loadPcd((*streamIterator).string());
+        cityBlock(viewer, pointProcessorI, inputCloudI);
+
+        streamIterator++;
+        if(streamIterator == stream.end())
+            streamIterator = stream.begin();
+
         viewer->spinOnce ();
     } 
 }
